e20fe8a9a6293a73d7187eacec9c30e464adae0e
Ali Zayed Soltan Nhshl <128501418+Eng-Nhshl@users.noreply.github.com>
2026-09-04 23:45:20 +0300

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length !== t.length) return false;
+        const hashMap = new Array(26).fill(0);
+        for (let i = 0; i < s.length; i++) {
+            hashMap[s.charCodeAt(i) - "a".charCodeAt(0)]++;
+            hashMap[t.charCodeAt(i) - "a".charCodeAt(0)]--;
+        }
+        return hashMap.every((val) => val === 0);
+    }
+}
