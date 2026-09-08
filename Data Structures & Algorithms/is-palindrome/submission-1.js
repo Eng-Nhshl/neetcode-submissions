9d78d6b18324df412ff205e26bd4a24c6ceebbdc
Ali Zayed Soltan Nhshl <128501418+Eng-Nhshl@users.noreply.github.com>
2026-09-09 00:59:19 +0300

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        let left = 0;
+        let right = s.length - 1;
+
+        while (left < right) {
+            while (left < right && !this.isAlphanumirc(s[left])) left++;
+            while (right > left && !this.isAlphanumirc(s[right])) right--;
+            if (s[left].toLowerCase() !== s[right].toLowerCase()) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    /**
+     * @param {char} c
+     * @return {boolean}
+     */
+    isAlphanumirc(c) {
+        return (c >= "A" && c <= "Z") || (c >= "a" && c <= "z") || (c >= "0" && c <= "9");
+    }
+}
